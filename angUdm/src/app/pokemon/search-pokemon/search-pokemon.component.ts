61f3ef7b511d366e215c66620dde263cc45e772a
Amine <abdelamine.mehdaoui@gmail.com>
2020-09-07 10:55:05 +0200

    search feature done

--- angUdm/src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ angUdm/src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+import { Observable,Subject, of} from 'rxjs';
+import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators';
+import { PokemonService} from '../services/pokemon.service';
+import { Pokemon} from '../models/pokemon.model';
 
 @Component({
   selector: 'app-search-pokemon',
@@ -6,10 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-pokemon.component.scss']
 })
 export class SearchPokemonComponent implements OnInit {
-
-  constructor() { }
+  private searchTerms = new Subject<string>();
+  pokemons$: Observable<Pokemon[]>;
+  constructor(private pokemonService: PokemonService,
+              private router: Router) { }
+  /*ajoute un term de recherche dans le flux de l'observable 'searchterms"*/
+  search(term:string): void{
+    this.searchTerms.next(term);
+  }
 
   ngOnInit(): void {
+    this.pokemons$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string)=>
+        this.pokemonService.searchPokemons(term)
+      )
+    );
+  }
+  gotoDetail(pokemon:Pokemon):void{
+    let link = ['/pokemon', pokemon.id];
+    this.router.navigate(link);
   }
 
 }
